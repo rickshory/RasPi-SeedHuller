075927cdd8e355fd8f8f36ca1c2e529d1d28e706
Rick Shory <rickshory@gmail.com>
2022-02-12 02:59:22 +0000

    actually rotates motor

--- 22-02-10 stepper motor tests/test03 28BYJ-48.py
+++ 22-02-10 stepper motor tests/test03 28BYJ-48.py
@@ -9,17 +9,18 @@ stepper_pins = [5, 6, 13, 26]
 for p in stepper_pins:
     GPIO.setup(p, GPIO.OUT)
 
-for n in range(10):
+for n in range(1000):
     
     for a in range(4):
         for i, p in enumerate(stepper_pins):
-            print("n={}, a={}, i={}, p={}".format(n,a,i,p))
+#            print("n={}, a={}, i={}, p={}".format(n,a,i,p))
             if i == a:
                 GPIO.output(p, GPIO.HIGH)
             else:
                 GPIO.output(p, GPIO.LOW)
-        print("pause")
-        time.sleep(0.5)
-        
+#        print("pause")
+        time.sleep(0.002)
+
+print("done")
 GPIO.cleanup()
     
