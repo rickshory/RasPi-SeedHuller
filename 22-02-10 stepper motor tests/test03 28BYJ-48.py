6c61609c1ee1817bdd1a5f35c950b85722e783c9
Rick Shory <rickshory@gmail.com>
2022-02-12 02:56:47 +0000

    working demo

--- 22-02-10 stepper motor tests/test03 28BYJ-48.py
+++ 22-02-10 stepper motor tests/test03 28BYJ-48.py
@@ -10,14 +10,16 @@ for p in stepper_pins:
     GPIO.setup(p, GPIO.OUT)
 
 for n in range(10):
-    time.sleep(0.5)
+    
     for a in range(4):
         for i, p in enumerate(stepper_pins):
+            print("n={}, a={}, i={}, p={}".format(n,a,i,p))
             if i == a:
                 GPIO.output(p, GPIO.HIGH)
             else:
                 GPIO.output(p, GPIO.LOW)
-    
+        print("pause")
+        time.sleep(0.5)
         
 GPIO.cleanup()
     
